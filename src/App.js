e64ce2e8e805aa8dabb7f8be35d7ebcc689a5af9
Sava <sava.vuckovic@quantox.com>
2019-01-15 12:34:30 +0100

    created the Title component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Title from './components/Title';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        app
+        <Title titleText="test" element="h2" color="red" bgColor="blue"/>
+        <Title titleText="test 2" color="red" bgColor="yellow"/>
       </div>
     );
   }
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
-export default function Title() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default function Title(props) {
+  const { element, titleText, color, bgColor } = props;
+  const styles = { color, backgroundColor: bgColor };
+
+  switch(element) {
+    case 'h1':
+      return <h1 style={styles}>{titleText}</h1>;
+    case 'h2':
+      return <h2 style={styles}>{titleText}</h2>;
+    case 'h3':
+      return <h3 style={styles}>{titleText}</h3>;
+    case 'h4':
+      return <h4 style={styles}>{titleText}</h4>;
+    case 'h5':
+      return <h5 style={styles}>{titleText}</h5>;
+    case 'h6':
+      return <h6 style={styles}>{titleText}</h6>;
+    default:
+      return <h1 style={styles}>{titleText}</h1>;
+  }
 }
 
