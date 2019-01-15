9281fc15cea7e29196f2155a0278599bf4d785cf
Sava <sava.vuckovic@quantox.com>
2019-01-15 12:50:34 +0100

    created Button component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Title from './components/Title';
+import Button from './components/Button';
 
 class App extends Component {
   render() {
@@ -7,6 +8,8 @@ class App extends Component {
       <div className="App">
         <Title titleText="test" element="h2" color="red" bgColor="blue"/>
         <Title titleText="test 2" color="red" bgColor="yellow"/>
+        <Button onClick={() => console.log('TEST')} btnText="neki tekst" isHref/>
+        <Button onClick={() => console.log('TEST 2')} btnText="button"/>
       </div>
     );
   }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function Button() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default function Button(props) {
+  const { onClick, btnText, color, bgColor, isHref } = props;
+  const styles = { color, backgroundColor: bgColor };
+
+  return (isHref) 
+    ? <a href="#" onClick={onClick} style={styles}>{btnText}</a>
+    : <button onClick={onClick} style={styles}>{btnText}</button>;
 }
